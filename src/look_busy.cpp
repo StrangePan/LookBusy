53ef9d0d796ce4318cca8fad83c9ae90cd1f81e6
Deaboy <danieleandrus@gmail.com>
2016-02-02 09:52:53 -0700

    Added better timing mechanisms

--- src/look_busy.cpp
+++ src/look_busy.cpp
@@ -1,8 +1,12 @@
 #include "look_busy.h"
 
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 #include <thread>
 #include <chrono>
+
+using namespace std;
 
 look_busy::look_busy()
   : pg()
@@ -17,6 +21,8 @@ look_busy::~look_busy()
 
 int look_busy::run(int argc, char** argv)
 {
+  srand(time(0));
+  
   pg.load_action_strings("res/actions.txt");
   pg.load_adjective_strings("res/adjectives.txt");
   pg.load_noun_strings("res/nouns.txt");
@@ -24,8 +30,19 @@ int look_busy::run(int argc, char** argv)
   
   while (true)
   {
-    std::cout << pg.generate_phrase() << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(2));
+    string phrase = pg.generate_phrase();
+    
+    if (rand() % 5 > 0)
+    {
+      // 1:5 chance for long-running process
+    }
+    else
+    {
+      // 4:5 chance of random-length short-term process
+    }
+    
+    cout << pg.generate_phrase() << endl;
+    this_thread::sleep_for(chrono::seconds(2));
   }
   
   return 0;
